c3c75d6924500e80ebca661cbd1d214b7cc984d9
HugoCleret <hugocleret@hotmail.fr>
2019-09-19 16:19:24 +0200

    Ajoutez des fichiers projet.

---
+++ ModelMetier/cheval.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelMetier
+{
+    class Cheval
+    {
+        private int idCheval; 
+        // idCheval - HCH4pppddsdsdsdszdzdzdz
+         
+    }
+}
